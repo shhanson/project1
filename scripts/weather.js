c1786a037dea3816113f1acb01dc877e9ff6793d
Sarah Hanson <sarahh.hanson@gmail.com>
2017-05-15 15:29:48 -0500

    Form work

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,28 +1,73 @@
 $(document).ready(function() {
-    console.log("Hello from jQuery land!");
 
-    //Error checking for each AJAX call
-    $.ajaxSetup({
-        error: function(xhr, status, error) {
-            console.log(`An AJAX error occurred: ${xhr.status} ${error}.`)
-            return;
+
+    $("#coldmax").change(function(){
+        $("#coolmin").val(parseInt($("#coldmax").val())+1);
+        $("#coolmax").val(parseInt($("#coolmin").val())+1);
+        $("#warmmin").val(parseInt($("#coolmax").val())+1);
+    });
+
+
+    $("#coolmax").change(function(){
+        if(parseInt($("#coolmax").val()) < parseInt($("#coolmin").val())){
+            //$("#coolmax").addClass("red");
+            Materialize.toast(`Please enter a value greater than ${$("#coolmin").val()}`, 4000);
+        } else {
+            $("#warmmin").val(parseInt($("#coolmax").val())+1);
         }
     });
 
-    let $xhr_google = $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=78728&key=AIzaSyBB6SAGxzrKyG3TEhlDJDLNfulXhPguo3M");
-
-    $xhr_google.done(function(google_data){
-        console.log("GOOGLE API REQUEST NO PROBLEM!");
-        //console.log(google_data);
-
+    $("#warmmax").change(function(){
+        if(parseInt($("#warmmax").val()) < parseInt($("#warmmin").val())){
+            //$("#coolmax").addClass("red");
+            Materialize.toast(`Please enter a value greater than ${$("#warmmin").val()}`, 4000);
+        } else {
+            $("#hotmax").val(parseInt($("#warmmax").val())+1);
+        }
 
     });
 
-    let $xhr_darksky = $.getJSON("https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/30.267153,-97.743061");
 
-    $xhr_darksky.done(function(darksky_data) {
-
-        console.log("DARKSKY API REQUEST NO PROBLEM!");
-        //console.log(data);
+    //Listener for "Clear" button
+    $("#clear").click(function() {
+        $("form").trigger("reset");
     });
+
+    //Listner for form submission
+    $("form").submit(function(event) {
+        event.preventDefault();
+        processForm();
+    });
+
+
+    function processForm() {
+
+        let postInfo = {};
+        let $nickname = $("nickname").val();
+
+
+        //Error checking for each AJAX call
+        $.ajaxSetup({
+            error: function(xhr, status, error) {
+                console.log(`An AJAX error occurred: ${xhr.status} ${error}.`)
+                return;
+            }
+        });
+
+        let $xhr_google = $.getJSON("http://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=78728&key=AIzaSyBB6SAGxzrKyG3TEhlDJDLNfulXhPguo3M");
+
+        $xhr_google.done(function(googleData) {
+            // console.log("GOOGLE API REQUEST NO PROBLEM!");
+        });
+
+        let $xhr_darksky = $.getJSON("http://galvanize-cors-proxy.herokuapp.com/https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/30.267153,-97.743061");
+
+        $xhr_darksky.done(function(darkskyData) {
+
+            // console.log("DARKSKY API REQUEST NO PROBLEM!");
+        });
+
+    }
+
+
 });
