af73763a35ec7903f3002f0d1b384a8d49412484
Sarah Hanson <sarahh.hanson@gmail.com>
2017-05-15 10:47:01 -0500

    Added Google Maps API request

--- scripts/weather.js
+++ scripts/weather.js
@@ -9,11 +9,20 @@ $(document).ready(function() {
         }
     });
 
-    let $xhr = $.getJSON("https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/30.267153,-97.743061");
+    let $xhr_google = $.getJSON("https://maps.googleapis.com/maps/api/geocode/json?address=78728&key=AIzaSyBB6SAGxzrKyG3TEhlDJDLNfulXhPguo3M");
 
-    $xhr.done(function(data) {
+    $xhr_google.done(function(google_data){
+        console.log("GOOGLE API REQUEST NO PROBLEM!");
+        console.log(google_data);
 
-        console.log("API REQUEST NO PROBLEM!");
+
+    });
+
+    let $xhr_darksky = $.getJSON("https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/30.267153,-97.743061");
+
+    $xhr_darksky.done(function(darksky_data) {
+
+        console.log("DARKSKY API REQUEST NO PROBLEM!");
         //console.log(data);
     });
 });
