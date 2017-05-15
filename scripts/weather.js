610272d067e6386a816c6786221a65ba1ff089c9
Sarah Hanson <sarahh.hanson@gmail.com>
2017-05-15 17:03:56 -0500

    Form validation and Ajax work

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,50 +1,60 @@
 $(document).ready(function() {
 
-
-    $("#coldmax").change(function(){
-        $("#coolmin").val(parseInt($("#coldmax").val())+1);
-        $("#coolmax").val(parseInt($("#coolmin").val())+1);
-        $("#warmmin").val(parseInt($("#coolmax").val())+1);
+    //Listener for #coldmax field. Populates other field ranges based on input.
+    $("#coldmax").change(function() {
+        $("#mildmin").val(parseInt($("#coldmax").val()) + 1);
+        $("#mildmax").val(parseInt($("#mildmin").val()) + 1);
+        $("#hotmax").val(parseInt($("#mildmax").val()) + 1);
     });
 
-
-    $("#coolmax").change(function(){
-        if(parseInt($("#coolmax").val()) < parseInt($("#coolmin").val())){
-            //$("#coolmax").addClass("red");
-            Materialize.toast(`Please enter a value greater than ${$("#coolmin").val()}`, 4000);
+    //Listener for #mildmax field. Populates other field ranges based on input.
+    $("#mildmax").change(function() {
+        if (parseInt($("#mildmax").val()) <= parseInt($("#mildmin").val())) {
+            Materialize.toast(`Please enter a value greater than ${$("#mildmin").val()}`, 4000);
         } else {
-            $("#warmmin").val(parseInt($("#coolmax").val())+1);
+            $("#hotmax").val(parseInt($("#mildmax").val()) + 1);
         }
     });
-
-    $("#warmmax").change(function(){
-        if(parseInt($("#warmmax").val()) < parseInt($("#warmmin").val())){
-            //$("#coolmax").addClass("red");
-            Materialize.toast(`Please enter a value greater than ${$("#warmmin").val()}`, 4000);
-        } else {
-            $("#hotmax").val(parseInt($("#warmmax").val())+1);
-        }
-
-    });
-
 
     //Listener for "Clear" button
     $("#clear").click(function() {
         $("form").trigger("reset");
     });
 
-    //Listner for form submission
+    //Listener for form submission
     $("form").submit(function(event) {
         event.preventDefault();
         processForm();
     });
 
-
+    //Function to validate and process form input
     function processForm() {
 
-        let postInfo = {};
-        let $nickname = $("nickname").val();
+        let formData = {
+            nickname: $("#nickname").val(),
+            zipcode: $("#zipcode").val(),
+            units: $("input[name=units]:checked").val(),
+            coldMax: parseInt($("#coldmax").val()),
+            mildMin: parseInt($("#mildmin").val()),
+            mildMax: parseInt($("#mildmax").val()),
+            hotMax: parseInt($("#hotmax").val())
+        };
 
+        //console.log(formInfo);
+
+        if (formData.mildMin > formData.coldMax && formData.mildMax > formData.mildMin && formData.hotMax > formData.mildMax && (/(^\d{5}$)|(^\d{5}-\d{4}$)/).test(formData.zipcode)){
+            ajaxCalls(formData);
+        } else {
+            Materialize.toast("Input error!", 4000);
+            return;
+        }
+
+    }//END processForm
+
+    function ajaxCalls(formData) {
+
+        let longitude;
+        let latitude;
 
         //Error checking for each AJAX call
         $.ajaxSetup({
@@ -54,20 +64,29 @@ $(document).ready(function() {
             }
         });
 
-        let $xhr_google = $.getJSON("http://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=78728&key=AIzaSyBB6SAGxzrKyG3TEhlDJDLNfulXhPguo3M");
+        let $xhr_google = $.getJSON(`http://galvanize-cors-proxy.herokuapp.com/https://maps.googleapis.com/maps/api/geocode/json?address=${formData.zipcode}&key=AIzaSyBB6SAGxzrKyG3TEhlDJDLNfulXhPguo3M`);
+
 
         $xhr_google.done(function(googleData) {
             // console.log("GOOGLE API REQUEST NO PROBLEM!");
+            //console.log(googleData);
+            latitude = googleData.results[0].geometry.location.lat;
+            longitude = googleData.results[0].geometry.location.lng;
+
+            let $xhr_darksky = $.getJSON(`http://galvanize-cors-proxy.herokuapp.com/https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/${latitude},${longitude}`);
+
+            $xhr_darksky.done(function(darkskyData) {
+
+                //console.log("DARKSKY API REQUEST NO PROBLEM!");
+                //console.log(darkskyData);
+            });
+
+
         });
 
-        let $xhr_darksky = $.getJSON("http://galvanize-cors-proxy.herokuapp.com/https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/30.267153,-97.743061");
 
-        $xhr_darksky.done(function(darkskyData) {
 
-            // console.log("DARKSKY API REQUEST NO PROBLEM!");
-        });
-
-    }
+    } //END ajaxCalls
 
 
 });
