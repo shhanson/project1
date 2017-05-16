c9f0b60159226d61989c1e94c63c07c03417bf7e
Sarah Hanson <sarahh.hanson@gmail.com>
2017-05-16 15:30:23 -0500

    Worked on displaying results after form submission

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,18 +2,21 @@ $(document).ready(function() {
 
     const ZIP_REGEX = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
     const NICKNAME_REGEX = /^[A-Za-z0-9_\-]{4,}$/;
+    const MONTHS = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    const DAYS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-    $("#nickname").change(function(){
-        if(!NICKNAME_REGEX.test($("#nickname").val())){
+    //Listener for "validation" of #nickname field
+    $("#nickname").change(function() {
+        if (!NICKNAME_REGEX.test($("#nickname").val())) {
             Materialize.toast('Nickname must be at least 4 characters in length, alphanumeric, "-" and "_" allowed', 4000);
         }
     });
 
-    $("#zipcode").change(function(){
-        if(!ZIP_REGEX.test($("#zipcode").val())){
+    //Listener for "validation" of #zipcode field
+    $("#zipcode").change(function() {
+        if (!ZIP_REGEX.test($("#zipcode").val())) {
             Materialize.toast('Invalid ZIP code format', 4000);
         }
-
     });
 
     //Listener for #coldmax field. Populates other field ranges based on input.
@@ -41,14 +44,40 @@ $(document).ready(function() {
     $("form").submit(function(event) {
         event.preventDefault();
         let formData = processForm();
-
-
         ajaxCalls(formData);
+    });
+
+    function displayResults(formData, tempData) {
+
         $("#page1").toggle();
+
+        let symbol = ((formData.units === "Celcius") ? "&#8451" : "&#8457");
+        let date = new Date();
+        let currDate = `${DAYS[date.getDay()]} ${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+        let currMinutes = date.getMinutes();
+        if(currMinutes < 10){
+            currMinutes = "0" + currMinutes;
+        }
+        let currTime = `${date.getHours()}:${currMinutes}`
+
+
+
+        let $mainCol = $("#resultpage div:first-child");
+        let $innerCol = $("<div class='col s12 white'></div>");
+        let $outfitContainer = $("<img src='http://placehold.it/450x450'>");
+        let $infoDiv = $(`<div class='col s12 white'><div class='row'> <div class='col s6'><p><strong>Currently:</strong><br> ${currDate} @ ${currTime}</p><h2>${tempData.currentTemp} ${symbol}</h2></div> <div class='col s6'><p><strong>Summary:</strong><br>High: ${tempData.highTemp.temp}${symbol} @ ${tempData.highTemp.timestamp}, Low: ${tempData.lowTemp.temp}${symbol} @ ${tempData.lowTemp.timestamp}</p></div>   </div></div>`);
+
+        $mainCol.append(`<h4 class="center-align">${formData.nickname} you should wear...</h4>`);
+        $mainCol.append($innerCol);
+        $innerCol.append($outfitContainer);
+        $innerCol.append($infoDiv);
+
+
+
+
         $("#resultpage").toggle();
 
-
-    });
+    }
 
     //Function to validate and process form input
     function processForm() {
@@ -64,20 +93,19 @@ $(document).ready(function() {
         };
 
         //Validating the fields before submission again JUST IN CASE
-        if (formData.mildMin > formData.coldMax && formData.mildMax > formData.mildMin && formData.hotMax > formData.mildMax && ZIP_REGEX.test(formData.zipcode) && NICKNAME_REGEX.test(formData.nickname)){
+        if (formData.mildMin > formData.coldMax && formData.mildMax > formData.mildMin && formData.hotMax > formData.mildMax && ZIP_REGEX.test(formData.zipcode) && NICKNAME_REGEX.test(formData.nickname)) {
             return formData;
         } else {
             Materialize.toast("Input error!", 4000);
             return;
         }
-
-
-    }//END processForm
+    } //END processForm
 
     function ajaxCalls(formData) {
 
         let longitude;
         let latitude;
+        let tempData;
 
         //Error checking for each AJAX call
         $.ajaxSetup({
@@ -92,24 +120,88 @@ $(document).ready(function() {
 
         $xhr_google.done(function(googleData) {
             // console.log("GOOGLE API REQUEST NO PROBLEM!");
-            console.log(googleData);
+            //console.log(googleData);
             latitude = googleData.results[0].geometry.location.lat;
             longitude = googleData.results[0].geometry.location.lng;
 
             let $xhr_darksky = $.getJSON(`http://galvanize-cors-proxy.herokuapp.com/https://api.darksky.net/forecast/4425e1c1ccfdf34f99ecc35f208760b3/${latitude},${longitude}`);
 
             $xhr_darksky.done(function(darkskyData) {
+                tempData = {
+                    currentTemp: Math.round(darkskyData.currently.apparentTemperature),
+                    currentIcon: darkskyData.currently.icon,
+                    dayIcon: darkskyData.daily.icon,
+                    highTemp: getHighTemp(darkskyData.hourly.data),
+                    lowTemp: getLowTemp(darkskyData.hourly.data)
+                };
+                //console.log(tempData.highTemp);
+                //console.log(tempData.lowTemp);
 
-                //console.log("DARKSKY API REQUEST NO PROBLEM!");
-                //console.log(darkskyData);
+                displayResults(formData, tempData);
+
             }); //END $xhr_darksky.done
-
 
         }); //END $xhr_google.done
 
-
-
+        return tempData;
     } //END ajaxCalls
+
+    function getHighTemp(array) {
+        let forecastObj = {};
+        let maxTemp = Number.MIN_SAFE_INTEGER;
+        let maxTempTime;
+        for (let i = 0; i < 24; i++) {
+            if (array[i].apparentTemperature > maxTemp) {
+                maxTemp = array[i].apparentTemperature;
+                maxTempTime = array[i].time;
+            }
+        }
+
+        forecastObj.temp = Math.round(maxTemp);
+        //let dateFormat = require('dateformat');
+        let date = new Date(maxTempTime * 1000);
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        if(minutes < 10){
+            minutes = "0" + minutes;
+        }
+
+        forecastObj.timestamp = `${hour}:${minutes}`
+        //console.log(forecastObj);
+
+
+
+
+        //console.log(forecastObj);
+
+        return forecastObj;
+    }
+
+    function getLowTemp(array) {
+        let forecastObj = {};
+        let minTemp = Number.MAX_SAFE_INTEGER;
+        let minTempTime;
+        for (let i = 0; i < 24; i++) {
+            if (array[i].apparentTemperature < minTemp) {
+                minTemp = array[i].apparentTemperature;
+                minTempTime = array[i].time;
+            }
+        }
+
+        forecastObj.temp = Math.round(minTemp);
+
+        let date = new Date(minTempTime * 1000);
+        let hour = date.getHours();
+        let minutes = date.getMinutes();
+        if(minutes < 10){
+            minutes = "0" + minutes;
+        }
+
+        forecastObj.timestamp = `${hour}:${minutes}`
+        //console.log(forecastObj);
+
+        return forecastObj;
+    }
 
 
 });
